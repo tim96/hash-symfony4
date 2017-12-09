786012a7a2282cd9f3a21c1787c46c308d1d33a0
Alexander Timofeev <tim96@yandex.ru>
2017-12-10 02:45:08 +0500

    Added new routes for about, and base64 actions.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Dto\Base64Dto;
 use App\Dto\HashDto;
+use App\Form\Base64Type;
 use App\Form\HashType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,5 +27,30 @@ class IndexController extends Controller
             'form' => $form->createView(),
             'result' => $result
         ]);
+    }
+
+    public function about()
+    {
+        return $this->render('index/about.html.twig');
+    }
+
+    public function base64(Request $request)
+    {
+        $result = null;
+        $form = $this->createForm(Base64Type::class, new Base64Dto());
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            /** @var Base64Dto $data */
+            $data = $form->getData();
+
+            if ($data->isConvertFromBase64()) {
+                $result = base64_decode($data->getText());
+            } else {
+                $result = base64_encode($data->getText());
+            }
+        }
+
+        return $this->render('index/base64.html.twig', ['form' => $form->createView(), 'result' => $result]);
     }
 }
