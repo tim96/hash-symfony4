30955b9dd0cbc3883d9a58de7a406b58152c6f4b
Alexander Timofeev <tim96@yandex.ru>
2017-12-18 20:38:32 +0100

    Using hash process form inside controller.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,11 +3,9 @@
 namespace App\Controller;
 
 use App\Dto\Base64Dto;
-use App\Dto\HashDto;
 use App\Dto\PasswordGenerateDto;
 use App\Dto\RandomStringDto;
 use App\Form\Base64Type;
-use App\Form\HashType;
 use App\Form\PasswordGenerateType;
 use App\Form\RandomStringType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,20 +13,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller
 {
-    public function hash(Request $request)
+    public function hash()
     {
-        $result = null;
-        $form = $this->createForm(HashType::class, new HashDto());
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var HashDto $data */
-            $data = $form->getData();
-            $result = $this->get('manager.hash')->generate($data);
-        }
+        $manager = $this->get('process.form.hash');
+        $result = $manager->handle();
 
         return $this->render('index/hash.html.twig', [
-            'form' => $form->createView(),
+            'form' => $manager->getForm()->createView(),
             'result' => $result
         ]);
     }
